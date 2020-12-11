d2cdde32269a7239de64ad27bfb5de09c2ad721e
xnmll <xnmlllcy@gmail.com>
2020-12-11 21:06:04 +0800

    add first

---
+++ src/main/java/cn/xnmll/demo_2020/Demo2020Application.java
@@ -0,0 +1,12 @@
+package cn.xnmll.demo_2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2020Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xnmll/demo_2020/Demo2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xnmll.demo_2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
